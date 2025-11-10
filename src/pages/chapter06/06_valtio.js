630b7d2985edc0783cb22c1577077e8f196a2ffe
iloveisyou <iloveisyou@hanmir.com>
2025-11-10 16:58:04 +0900

    11장end

--- src/pages/chapter06/06_valtio.js
+++ src/pages/chapter06/06_valtio.js
@@ -15,10 +15,16 @@ const TimerView = ({timer}) => {
   return (<button onClick={()=>timer.reset()}>Seconds passed: {snap.secondsPassed} </button>)
 };
 
+export const App6 = () => (<div><TimerView timer={myTimer} /></div>);
+
 
 // Valtio 예제 - Valtio가 허용하는 방식중에 다른형태로 코딩
 const timer2 = proxy({secondsPassed: 0});
 export const increase = () => { timer2.secondsPassed += 1; }
 export const reset = () => { timer2.secondsPassed = 0; }
 export const useSecondsPasses = () => useSnapshot(timer2).secondsPassed;
-export const App6 = () => (<div><TimerView timer={myTimer} /></div>);
+setInterval(()=>{
+  increase();
+}, 1000);
+export const App7 = () => (<div><button type="button" onClick={reset}> 상태객체에서 함수분리: {useSecondsPasses()}</button></div>);
+
